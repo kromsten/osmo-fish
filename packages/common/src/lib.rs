3b8ab5869d3fa7f9b8f0bde240958891dd095c83
Martin Kromsten <kromsten@pm.me>
2023-12-01 16:32:16 +0100

    init minter

---
+++ packages/common/src/lib.rs
@@ -0,0 +1,17 @@
+use cosmwasm_schema::cw_serde;
+
+#[cw_serde]
+pub struct MintData {
+    pub name: String,
+    pub image: String,
+}
+
+impl From<cw721_metadata_onchain::Extension> for MintData {
+    fn from(extension: cw721_metadata_onchain::Extension) -> Self {
+        let ext = extension.unwrap();
+        Self {
+            name: ext.name.unwrap(),
+            image: ext.image.unwrap(),
+        }
+    }
+}
